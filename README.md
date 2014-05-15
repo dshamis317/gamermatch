65508845777a34eee0b37de8d7cdd74904279125
dshamis317 <dshamis317@gmail.com>
2014-05-15 16:59:44 -0400

    Updated User table to reflect age and location

---
+++ README.md
@@ -0,0 +1,3 @@
+== README
+
+Gamer Match will find like minded gamers who share an interest in video games (and games owned...) to match them up for multiplayer game sessions. Through Gamer Match, gamers get to avoid the random encounters typically found through Steam, PSN and Xbox Live, and
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140515203659) do
+ActiveRecord::Schema.define(version: 20140515205432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20140515203659) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "age"
+    t.string   "location"
   end
 
 end
