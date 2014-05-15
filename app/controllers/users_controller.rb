4c0998181bbfe2a2258e3b9dd5a0950ec0c4e9fb
dshamis317 <dshamis317@gmail.com>
2014-05-15 19:46:12 -0400

    DB temporarily seeded with my data

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+
+  before_action:current_user
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(username: 'dshamis317', email: 'dshamis317@gmail.com', age: 27, location: 'New York, NY')
