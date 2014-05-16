8cc7d2bd02c4437c7b2f1171bfd851a06db93939
dshamis317 <dshamis317@gmail.com>
2014-05-16 19:45:55 -0400

    Nav bar working, saving game returns you to user profile

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,10 +8,12 @@ class GamesController < ApplicationController
   end
 
   def search
+    @user = current_user
     @games = Game.games_search(params["name"])
   end
 
   def show
+    @user = current_user
     @games = Game.game_page(params[:id])
   end
 
@@ -21,9 +23,10 @@ class GamesController < ApplicationController
   end
 
   def create
+    @user = current_user
     new_game = Game.create(params.require('game').permit(:title, :description, :release_date, :image_url, :publisher, :genre, :developer, :platform))
     @current_user.games << new_game
-    redirect_to '/users/'
+    redirect_to "/users/#{@user.id}/games"
   end
 
   def destroy
