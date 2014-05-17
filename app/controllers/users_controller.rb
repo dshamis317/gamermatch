e5d43d7f1c215bc30ee982fcfd94a936a659d014
dshamis317 <dshamis317@gmail.com>
2014-05-17 12:08:11 -0400

    create users complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,22 +2,35 @@ class UsersController < ApplicationController
 
   before_action:current_user
 
-# GET    /users(.:format)                         users#index
-  def index
-    @users = User.all
-    @user = current_user
-  end
+def index
+  @users = User.all
+  @user = current_user
+end
 
-# POST   /users(.:format)                         users#create
-# GET    /users/new(.:format)                     users#new
+def create
+  user = User.create(user_params)
+  redirect_to "/users"
+end
+
+def new
+  @user = current_user
+end
+
 # GET    /users/:id/edit(.:format)                users#edit
-# GET    /users/:id(.:format)                     users#show
-  def show
-    @user = User.find(params[:id])
-    @games = @user.games
-  end
-# PATCH  /users/:id(.:format)                     users#update
+
+def show
+  @user = User.find(params[:id])
+  @games = @user.games
+end
+
 # PUT    /users/:id(.:format)                     users#update
+
 # DELETE /users/:id(.:format)                     users#destroy
+
+private
+
+def user_params
+  params.require('user').permit(:username, :email, :age, :location, :phone_number)
+end
 
 end
