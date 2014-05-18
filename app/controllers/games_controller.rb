546133910f44a7daf924f7860a5214d33ce83392
dshamis317 <dshamis317@gmail.com>
2014-05-17 20:49:16 -0400

    A little css work

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,7 +24,6 @@ class GamesController < ApplicationController
   end
 
   def create
-    binding.pry
     @user = current_user
     new_game = Game.create(params.require('game').permit(:title, :description, :release_date, :image_url, :publisher, :genre, :developer, :platform))
     @current_user.games << new_game
