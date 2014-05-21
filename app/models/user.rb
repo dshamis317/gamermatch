fc683d49d40b010d6fe3a71f5b56916e1b469c32
dshamis317 <dshamis317@gmail.com>
2014-05-21 14:14:34 -0400

    formatting on users/index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :username, presence: true, uniqueness: true, :length => {:within => 8..20, :wrong_length => "needs to be between 8 and 20 characters"}
+  validates :username, presence: true, uniqueness: true, :length => {:within => 6..20, :wrong_length => "needs to be between 6 and 20 characters"}
   validates :age, presence: true
   validates :location, presence: true
   validates :phone_number, presence: true
